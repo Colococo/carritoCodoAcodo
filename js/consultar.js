6b775299feb96fc8a1ae7ba8c269af2167dc3c7a
colococo <jorge_mattii@hotmail.com>
2024-12-20 02:00:15 -0300

    fin del proyecto

--- js/consultar.js
+++ js/consultar.js
@@ -1,3 +1,6 @@
+// mensaje carrito
+const mensajeCarrito = document.getElementById('mensaje');
+
 const libros = [
     {
           "nombre" : "Lenguaje Java",
@@ -27,14 +30,62 @@ for(let i=0; i < libros.length; i++) {
             <section class="producto">
                 <h2>${libros[i].nombre}</h2>
                 <div class="libros"><img src=${libros[i].imagen}></div>
-                <a href="">Ver + Info</a>
                 <h2>Precio: $${libros[i].precio}</h2>
+                <input class="btn-info" type="button" value="Ver + Info">
                 <p>${libros[i].descripcion}</p>
-                <input class="agregarCarrito" id="btn-agrgar-carrito" type="button" value="Agregar al carrito">
+                <input class="btn-agregar-carrito" type="button" value="Agregar al carrito">
             </section>
     `;
-}
-
+};
 
 const contenedorLibros = document.getElementById("contenedorLibros");
 contenedorLibros.innerHTML = librosHtml;
+
+const btnAgregarCarrito = document.querySelectorAll('.btn-agregar-carrito');
+const listaCarrito = document.querySelector('#carrito ul');
+const totalCarrito = document.querySelector('#carrito p');
+
+let totalPagar = 0;
+
+// recorremos el areglo de los botones agregar carrito
+for(let i=0; i < btnAgregarCarrito.length; i++) {
+
+    function agregarLibroCarrito(){
+        const eLi = document.createElement("li");
+        eLi.innerText = `
+                Libro ${libros[i].nombre} $${libros[i].precio}
+        `;
+
+        listaCarrito.appendChild(eLi);
+
+        totalPagar += libros[i].precio;
+
+        totalCarrito.innerText = "Total a pagar: $" + totalPagar;
+
+        mensajeCarrito.innerText = "";
+    }
+
+    btnAgregarCarrito[i].addEventListener("click", agregarLibroCarrito)
+}
+
+function borrarCarrito() {
+    listaCarrito.innerText = "";
+    totalCarrito.innerText = "Total  a pagar: $0";
+    totalPagar = 0;
+    mensajeCarrito.innerText = "";
+};
+
+const btnBorrar = document.querySelector('#btn-borrar');
+btnBorrar.addEventListener('click', borrarCarrito);
+
+// boton pagar
+function pagarTotal(){
+    if(listaCarrito.innerText === "") {
+        mensajeCarrito.innerText = "Seleccionar un producto para abonar";
+    }else {
+        window.location.href = "/pagos.html";
+    }
+};
+
+const btnPagar = document.querySelector('#btn-pagar');
+btnPagar.addEventListener("click", pagarTotal);
